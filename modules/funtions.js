b9216304cebe589713ae43a6d00762015a05c18c
kris0395 <khrispitbrown@gmail.com>
2023-11-03 07:08:08 -0500

    cambios

--- modules/funtions.js
+++ modules/funtions.js
@@ -2,11 +2,13 @@ const urlValorant = "https://valorant-api.com/v1/agents"
 
 fetch(urlValorant)
 .then(response=>{
+    return response.json()
 
+}).then(
+    response=>console.table(response.data)
 
-}).catch(error=>{
-    
-
+).catch(error=>{
+    console.log("error", error)
 
 })
 
